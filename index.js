c92e3962fe929e467cafccd88e34c652d8846050
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-20 14:45:17 +1000

    proxy configuration

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express=require('express')
 const app=express()
 const test=require('./routes/test')
 
-app.use('/',test)
+app.use('/api',test)
 app.use(function(err,req,res,next){
     console.info(err.stack)
     res.status('500').send('server side went wrong')
 })
 
-app.listen('3000',function(){
-    console.log('listening on the port 3000')
+app.listen('3030',function(){
+    console.log('listening on the port 3030')
 }) 
