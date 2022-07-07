ecd7bcbf94abcd30300dd717d1d207a9e8b71165
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-07 23:02:37 +1000

    add one more attribute access

--- routes/service.js
+++ routes/service.js
@@ -3,9 +3,9 @@ const router=express.Router()
 const sqlFn=require('../mysql')
 
 router.post('/add',function(req,res,next){
-    const sql='insert into service values(null,?,?,?,?,?)'
-    const {snam,mname,category,capacity,icons}=req.body
-    const arr=[snam,mname,category,capacity,icons]
+    const sql='insert into service values(null,?,?,?,?,?,?)'
+    const {access,sname,mname,category,capacity,icons}=req.body
+    const arr=[access,sname,mname,category,capacity,icons]
     sqlFn(sql,arr,function(err,data){
         if(err){
             next(err)
@@ -26,9 +26,8 @@ router.get('/list',function(req,res,next){
             next(err)
             return
         }
-        if(data.affectedRows){
-            res.send({code:'00000',success:true})
-        }
+        res.send({code:'00000',records:data})
+        //res.send({code:'11111',records:data,message:'something went wrong'})
     })
 })
 
