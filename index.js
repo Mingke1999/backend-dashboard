0584e0652d93c429f80234a4b8b756237c5f745b
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-12 00:53:28 +1000

    location

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express=require('express')
 const app=express()
 //const test=require('./routes/test')
 const service=require('./routes/service')
+const area=require('./routes/location')
 const bodyParser=require('body-parser')
+
 
 //app.use('/api',test)
 app.use(bodyParser.json()) //middleware
 app.use('/api/service',service)
+app.use('/api/area',area)
+
 app.use(function(err,req,res,next){
     console.info(err.stack)
     res.status('500').send('server side went wrong')
---
+++ routes/location.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const router=express.Router()
+const sqlFn=require('../mysql')
+
+router.get('/state',function(req,res,next){
+    const sql='select * from `state`'
+    sqlFn(sql,[],function(err,data){
+        if(err){
+            next(err)
+            return
+        }
+        res.send({code:'00000',records:data})
+    })
+})
+
+router.get('/city',function(req,res,next){
+    const sql='select * from city where `pid`=?'
+    sqlFn(sql,[req.query.pid],function(err,data){
+        if(err){
+            next(err)
+            return
+        }
+        res.send({code:'00000',records:data})
+    })
+})
+
+module.exports=router;
