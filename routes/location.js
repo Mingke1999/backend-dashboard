4c53c6c0bff3e36898199039993dcfc85d8b6f1b
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-13 20:58:52 +1000

    no working

--- routes/location.js
+++ routes/location.js
@@ -15,12 +15,15 @@ router.get('/state',function(req,res,next){
 
 router.get('/city',function(req,res,next){
     const sql='select * from city where `pid`=?'
-    sqlFn(sql,[req.query.pid],function(err,data){
+    sqlFn(sql,[req.query.pid],function(err,data){ 
+        //console.log('req.query.pid')
+        //console.log(req.query.pid)//req.query.pid
         if(err){
             next(err)
             return
         }
         res.send({code:'00000',records:data})
+        
     })
 })
 
