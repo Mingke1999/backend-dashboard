11746c102ed1474803180f4db4fd85cced33fe57
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-14 21:42:34 +1000

    upload done location has error

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app=express()
 const test=require('./routes/test')
 const service=require('./routes/service')
 const area=require('./routes/location')
-const bodyParser=require('body-parser')
 const upload=require('./routes/uploads')
+const bodyParser=require('body-parser')
 
-app.use(express.static('./data'))
-app.use('/api',test)
+
+app.use(express.static('./data'))   //when visiting port 3030, directly go directory /data 
 app.use(bodyParser.json()) //middleware
+app.use('/api',test)
 app.use('/api/service',service)
 app.use('/api/area',area)
 app.use('/api/upload',upload)
