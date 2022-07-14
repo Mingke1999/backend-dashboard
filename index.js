c6e9a54760c545cc06b98d82ea4d5c22fd3186ec
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-14 18:20:18 +1000

    upload multer

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const test=require('./routes/test')
 const service=require('./routes/service')
 const area=require('./routes/location')
 const bodyParser=require('body-parser')
+const upload=require('./routes/uploads')
 
-
+app.use(express.static('./data'))
 app.use('/api',test)
 app.use(bodyParser.json()) //middleware
 app.use('/api/service',service)
 app.use('/api/area',area)
-
+app.use('/api/upload',upload)
 app.use(function(err,req,res,next){
     console.info(err.stack)
     res.status('500').send('server side went wrong')
